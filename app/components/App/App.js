8b1259d62e080d055f8f39afd99d5ccbc8ad7472
Nir Ben-Yair <nirbenya@gmail.com>
2017-10-03 00:24:21 +0300

    add sentry

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -28,6 +28,8 @@ export default class App extends Component {
         firebase.initializeApp(config);
 
         LocalStorageService.init();
+
+        Raven.config('https://cdc49c9d2b0c44f38b5b7911c8e58ddc@sentry.io/225081').install();
     };
 
     render(){
