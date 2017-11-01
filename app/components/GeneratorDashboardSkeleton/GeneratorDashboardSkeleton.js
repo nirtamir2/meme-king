ac5ea22b9c4ed543b8c3daa8983f2e169f8c1a21
Nir Ben-Yair <nirbenya@gmail.com>
2017-11-01 02:55:28 +0200

    change generator skeleton

--- app/components/GeneratorDashboardSkeleton/GeneratorDashboardSkeleton.js
+++ app/components/GeneratorDashboardSkeleton/GeneratorDashboardSkeleton.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default () => (
     <div className="box-generator-skeleton col-lg-5 col-xs-12">
+        <div className="flex space-between btns-wrapper visible-mobile-flex">
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+
+        </div>
         <div className="meme-text-fields-container">
             <div className=" meme-text-field">
                 <div className="item input-field"/>
@@ -14,11 +23,15 @@ export default () => (
             </div>
         </div>
 
-        <div className="item generator-btn-field" />
-        <div className="item generator-btn-field" />
-        <div className="item generator-btn-field" />
-        <div className="item generator-btn-field" />
-        <div className="item generator-btn-field" />
+        <div className="hidden-mobile">
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+            <div className="item generator-btn-field" />
+        </div>
+
 
     </div>
 )
