dd7f37db1e18a63d24dddc55b218abd9f98a2219
Nir Ben-Yair <nirbenya@gmail.com>
2018-01-17 16:17:30 +0200

    add fullstory

--- app/components/Icon/Icon.js
+++ app/components/Icon/Icon.js
@@ -7,7 +7,10 @@ import iconConstants from 'constants/iconConstants';
 const Icon =  ({ name, top, className, onClick, isRound, size, theme, isActive }) => {
 
     return (
-        <i style={{top : top}} onClick={onClick} className={classNames('box-icon',`size-${size}`,theme, { 'active' : isActive }, { 'round': isRound }, iconConstants[name], className)} />
+        <i style={{top : top}}
+           onClick={onClick}
+           className={classNames('box-icon',`size-${size}`,theme, { 'active' : isActive }, { 'round': isRound }, iconConstants[name], className)}
+        />
     )
 }
 
