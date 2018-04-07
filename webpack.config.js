31db34f33227f29a93c41f52d7fba98fd593f61f
Nir Ben-Yair <nirbenya@gmail.com>
2018-04-07 14:09:48 +0300

    build add env === production

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports =  function({ env }) {
         plugins: [
             new webpack.DefinePlugin({
                 'ENV': JSON.stringify(env),
-                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+                'process.env.NODE_ENV': isProduction ? JSON.stringify('production') : 'development'
             }),
             isProduction ? new UglifyJSPlugin() : _.noop,
             new ExtractTextPlugin('style.css'),
